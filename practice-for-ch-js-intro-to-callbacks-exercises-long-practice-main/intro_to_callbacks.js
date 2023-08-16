14e70050a0d637c209a165adb7b4533cec911e42
ajb-4 <alexbrown4711@gmail.com>
2023-08-16 15:06:09 -0400

    write tick and other helpers

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/intro_to_callbacks.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/intro_to_callbacks.js
@@ -1,24 +1,33 @@
 class Clock {
     constructor() {
         // 1. Create a Date object.
-        const currentDateTime = String(new Date());
+        const currentDateTime = new Date();
         // 2. Store the hours, minutes, and seconds.
-        const strTime = currentDateTime.split(" ")[4].split(":")
-        let intTime = []
-        strTime.forEach(function(el) {
-            intTime.push(parseInt(el))
-        })
+        this.seconds = currentDateTime.getSeconds();
+        this.minutes = currentDateTime.getMinutes();
+        this.hours = currentDateTime.getHours();
         // 3. Call printTime.
+        this.printTime() //object style? / method?
         // 4. Schedule the tick at 1 second intervals.
+        setInterval(_tick, 1000); //function style?
     }
 
     printTime() {
         // Format the time in HH:MM:SS
+        const formattedTime = `${this.seconds}:${this.minutes}:${this.hours}`;
         // Use console.log to print it.
+        console.log(formattedTime);
     }
 
     _tick() {
         // 1. Increment the time by one second.
+    
         // 2. Call printTime.
+            
     }
+
+    addMinutes () {
+
+    }
+
 }
